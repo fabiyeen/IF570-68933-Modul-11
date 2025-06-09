bc3279cbe627d2f223e88a9f17c7290e377455a6
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-06-09 23:31:46 +0700

    fifth commit

--- app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -6,6 +6,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import org.junit.Test
+import kotlin.test.assertEquals
 
 class RaceParticipant(
     val name: String,
@@ -25,7 +31,35 @@ class RaceParticipant(
             }
         } catch (e: CancellationException) {
             Log.e("RaceParticipant", "$name: ${e.message}")
-            throw e // Always re-throw CancellationException.
+            throw e
         }
+    }
+}
+
+class RaceParticipantTest {
+
+    private val raceParticipant = RaceParticipant(
+        name = "Test",
+        maxProgress = 100,
+        progressDelayMillis = 10L,
+        initialProgress = 0,
+        progressIncrement = 1
+    )
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(100, raceParticipant.currentProgress)
     }
 }
